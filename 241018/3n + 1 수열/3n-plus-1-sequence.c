ce76e94732d28b6ed85985fd5496b598720ececc
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-10-18 20:20:52 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/3n + 1 수열/3n-plus-1-sequence.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt=0;
+    scanf("%d", &n);
+
+    while (n!=1){
+        if (n%2==0){
+            n = n/2;
+        }
+        else {
+            n = 3*n+1;
+        }
+        cnt++;
+    }
+    printf("%d", cnt);
+    
+    return 0;
+}
