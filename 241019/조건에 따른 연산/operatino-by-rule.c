62bfdd607e9f82cd1d3bcee4385fcf9ec3f2c56b
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-10-19 02:53:49 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241019/조건에 따른 연산/operatino-by-rule.c
+++ 241019/조건에 따른 연산/operatino-by-rule.c
@@ -7,12 +7,13 @@ int main() {
 
     while (n < 1000){
         if (n%2==0) {
-            n=n*3+1;
+            n=(3*n)+1;
         } else {
-            n=n*2+1;
+            n=(2*n)+2;
         }
         cnt++;
     }
+    
     printf("%d", cnt);
     return 0;
 }
