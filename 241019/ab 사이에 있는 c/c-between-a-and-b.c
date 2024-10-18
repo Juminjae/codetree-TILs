f396d7921c34053e743cc0ea065c143776fce355
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-10-19 03:05:20 +0900

    Title: ab 사이에 있는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241019/ab 사이에 있는 c/c-between-a-and-b.c
+++ 241019/ab 사이에 있는 c/c-between-a-and-b.c
@@ -8,9 +8,9 @@ int main() {
     for (int i=a; i<=b; i++){
         if (i%c==0){
             printf("YES");
-            break;
+            return 0;
         }
     }
-    
+    printf("NO");
     return 0;
 }
