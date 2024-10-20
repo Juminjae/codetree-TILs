242575b45ef21b5e6e3c5a4f2f0353add493d555
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-10-20 14:31:45 +0900

    Title: 별표 출력하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241020/별표 출력하기 5/print-star-5.c
+++ 241020/별표 출력하기 5/print-star-5.c
@@ -5,13 +5,12 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n-i; j++){
-            printf("*");
-        }
-        printf(" ");
-        for (int j=0; j<n-i; j++) {
-            printf("*");
+    for (int i=n; i>=1; i--){
+        for (int j=1; j<=i; j++){
+            for (int k=1; k<=i; k++){
+                printf("*");
+            }
+            printf(" ");    
         }
         printf("\n");
     }
