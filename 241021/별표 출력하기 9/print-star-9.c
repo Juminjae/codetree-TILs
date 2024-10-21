cbed8766dd93af83d1119ae59d30dc747316ea52
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-10-21 20:26:59 +0900

    Title: 별표 출력하기 9, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241021/별표 출력하기 9/print-star-9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+     for (int i = 0; i < n; i++) {
+        // 앞에 공백 출력 (n-i-1만큼 공백 추가)
+        for (int j = 0; j < n - i - 1; j++) {
+            printf("  ");
+        }
+        // 별 출력 (2*i+1 만큼)
+        for (int j = 0; j < 2 * i + 1; j++) {
+            printf("* ");
+        }
+        printf("\n");  // 줄바꿈
+    }
+    return 0;
+}
