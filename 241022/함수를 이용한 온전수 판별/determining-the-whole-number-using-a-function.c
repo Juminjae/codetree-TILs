6fa3d514a839a646318aa0c1cbcc2e15b19a9b6f
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-10-22 16:26:46 +0900

    Title: 함수를 이용한 온전수 판별, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241022/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int func(int num1, int num2);
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    func(a,b);
+
+    return 0;
+}
+
+int func(int num1, int num2){
+    int cnt = 0;
+    for (int i=num1; i<=num2; i++){
+        if (i%2==0 || i/10==5 || (i%3==0 && i%9!=0)){
+            cnt++;
+        } else{
+            break;
+        }
+    }
+    printf("%d", cnt);
+}
