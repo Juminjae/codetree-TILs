1dab7329bd24749273ee435413448eda87cf2963
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-10-22 22:39:30 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.c
+++ 241022/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
-int func(int num1, int num2);
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int a,b;
-    scanf("%d %d", &a, &b);
-    func(a,b);
+int func(int a, int b) {
+    int cnt = 0;
 
-    return 0;
-}
+    for (int i = a; i <= b; i++) {
 
-int func(int a, int b){
-    int cnt=0;
-    for (int i=a; i<=b; i++){
-        int sum=0, num=i;
+        if (i < 2) continue; // 2보다 작은 수는 소수가 아님
 
-        while (num > 0){
-            sum += num % 10;
-            num /= 10;
+        int is_prime = 1;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                is_prime = 0;
+                break;
+            }
         }
-        if (i%2==1 && sum%2==0){
-            cnt++;
+
+        // 자릿수 합 계산
+        int sum = 0, num = i;
+        while (num > 0) {
+            sum += num % 10; // 자릿수를 더함
+            num /= 10;       // 자릿수를 줄임
+        }
+
+        // 소수이면서 자릿수의 합이 짝수인 경우
+        if (is_prime && sum % 2 == 0) {
+            cnt++; // 카운트 증가
         }
     }
     printf("%d", cnt);
     return cnt;
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    func(a, b);
+    return 0;
 }
