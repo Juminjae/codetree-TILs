623b37cb795ab39c4320fa567ced6571913406de
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-10-27 17:30:49 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241027/별표 출력하기 12/print-star-12.c
+++ 241027/별표 출력하기 12/print-star-12.c
@@ -6,12 +6,13 @@ int main() {
     scanf("%d", &n);
 
     for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            if (i % 2 == 0 || (j % 2 == 0 && i < n - 2)) {
-                printf("* ");
-            } else {
-                printf("  ");  // 홀수 위치에는 공백
-            }
+        for (int j=0; j<i; j++){
+            printf("  ");
+        }
+        for (int k = i; k < n; k++) {
+            printf("* ");
+            if (k < n - 1) 
+                printf("  ");
         }
         printf("\n");
     }
