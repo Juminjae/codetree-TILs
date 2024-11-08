77a1cf0cd4acc0ff67d7b696deb16ebf5c91997c
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-11-08 23:39:21 +0900

    Title: 특이한 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241108/특이한 출력/special-print.c
+++ 241108/특이한 출력/special-print.c
@@ -2,11 +2,11 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n, j;
+    int n;
     scanf("%d", &n);
 
     for (int i=1; i<=n; i++){
-        for (j=1; j<=n; j++){
+        for (int j=1; j<=n; j++){
             printf("(%d, %d) ", i, j);
             if ((i+j)%4==0){
                 printf("\n");
