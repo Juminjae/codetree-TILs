fe24a0e9f34a3116689d728cf0bf8862c0ea99b7
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-11-09 19:55:06 +0900

    Title: 19단 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241109/19단 출력/nineteen-times-table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for (int i=1; i<=19; i++){
+        for (int j=1; j<=19; j++){
+            printf("%d * %d = %d", i, j, i*j);
+            if (j % 2 == 1 && j != 19) {
+                printf(" / ");
+            } else {
+                printf("\n");
+            }
+        }   
+    }
+    return 0;
+}
