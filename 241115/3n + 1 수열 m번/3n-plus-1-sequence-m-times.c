3bad3ddf0ce242c77454e580ecb6c2f1c86c68f6
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-11-15 12:13:35 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241115/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
+++ 241115/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
@@ -2,10 +2,11 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int m, n, cnt=0;
+    int m, n;
     scanf("%d", &m);
 
     for (int i=0; i<m; i++){
+        int cnt = 0;
         scanf("%d", &n);
         while (n!=1){
             if (n%2==0){
@@ -15,7 +16,6 @@ int main() {
             }
             cnt++;
         }
-        cnt=0;
         printf("%d\n", cnt);
     }
   
