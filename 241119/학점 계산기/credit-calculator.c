43361147c07b6e645efe45c2e9e1452a937276b7
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-11-19 23:59:45 +0900

    Title: 학점 계산기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241119/학점 계산기/credit-calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+	scanf("%d", &n);
+
+	double val;
+	double sum_val = 0;
+
+	for (int i = 0; i < n; i++) {
+		scanf("%lf", &val);
+		sum_val += val;
+	}
+
+	printf("%lf", sum_val);
+	return 0;
+    return 0;
+}
