61e1b9efe683a65f5d70451d13a11d900ae13436
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-11-20 23:24:09 +0900

    Title: 배열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241120/배열의 특정 위치 찾기/find-specific-location-fo-array.c
+++ 241120/배열의 특정 위치 찾기/find-specific-location-fo-array.c
@@ -3,7 +3,7 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int n[10], sum=0,sum_1=0, count=0;
-    float avg=0;
+    float avg=0.0;
     for (int i=0; i<10; i++){
         scanf("%d", &n[i]);
 
@@ -13,7 +13,7 @@ int main() {
         if ((i+1)%3==0){
             sum += n[i];
             count++;
-            avg = sum/count;
+            avg = (float)sum/count;
         }
     }
     printf("%d %.1f",sum_1, avg);
