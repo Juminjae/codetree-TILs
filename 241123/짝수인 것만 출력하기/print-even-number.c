9a07143aaf77fd6b2b42293d24e5f9bf9bed2be1
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-11-23 23:42:44 +0900

    Title: 짝수인 것만 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241123/짝수인 것만 출력하기/print-even-number.c
+++ 241123/짝수인 것만 출력하기/print-even-number.c
@@ -7,7 +7,7 @@ int main() {
     for (int i=0; i<n; i++){
         scanf("%d", &arr[i]);
         if (arr[i]%2==0){
-            printf("%d", arr[i]);
+            printf("%d ", arr[i]);
         }
     }
 
