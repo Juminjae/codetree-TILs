dbdc903c6e41370de97ad1707715b2cb1a91565f
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-12-11 15:12:44 +0900

    Title: 십의 자리 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241211/십의 자리 숫자/number-of-tens-digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, arr[100], cnt[9]={0};
+    for (int i=0; i<10; i++){
+        scanf("%d ", &arr[i]);
+        if (arr[i]/=10){
+            cnt[arr[i]]++;
+        }
+    }
+
+    for (int i=1; i<10; i++){
+        printf("%d - %d\n", i, cnt[i]);
+    }
+    return 0;
+}
