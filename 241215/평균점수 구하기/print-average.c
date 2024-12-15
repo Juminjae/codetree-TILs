3c062273ae59304ce589b560d05feb287ed791d8
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-12-15 23:59:53 +0900

    Title: 평균점수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241215/평균점수 구하기/print-average.c
+++ 241215/평균점수 구하기/print-average.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     float n[10], sum=0.0;
     for (int i=0; i<8; i++){
-        scanf("%d ", &n[i]);
+        scanf("%f ", &n[i]);
         sum += n[i];
     }
     float avg = sum / 8;
