f794902bc98cc65664917a4d09e6deef2b33a668
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-12-28 23:56:21 +0900

    Title: 이상한 수열, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241228/이상한 수열/a-strange-sequence.py
@@ -0,0 +1,10 @@
+def sequence(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return sequence(3 // n) + sequence(n - 1)
+
+N = int(input("Enter N: "))
+print(sequence(N))
