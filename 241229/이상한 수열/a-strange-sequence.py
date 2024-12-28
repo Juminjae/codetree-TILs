9cd905e7fb03095f475d0e61cafb55161587c7d3
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-12-29 00:41:28 +0900

    Title: 이상한 수열, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241229/이상한 수열/a-strange-sequence.py
+++ 241229/이상한 수열/a-strange-sequence.py
@@ -1,10 +1,10 @@
+n = int(input())
+
 def sequence(n):
     if n == 1:
         return 1
-    elif n == 2:
+    if n == 2:
         return 2
-    index = max(1, 3 // n)
     return sequence(index) + sequence(n - 1)
 
-N = int(input())
-print(sequence(N))
+print(sequence(n))
