4a848b406df9b44c896d1c5a944561da39084e63
James Ju <45286407+Juminjae@users.noreply.github.com>
2024-12-29 00:41:21 +0900

    Title: 이상한 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241229/이상한 수열/a-strange-sequence.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// 수열을 계산하는 함수
+int sequence(int n) {
+    // 기본 조건
+    if (n == 1) {
+        return 1;
+    } else if (n == 2) {
+        return 2;
+    }
+    // ⌊ 3 / N ⌋ 값을 최소 1로 보정하여 계산
+    int index = 3 / n;
+    if (index < 1) {
+        index = 1;
+    }
+    return sequence(index) + sequence(n - 1);
+}
+
+int main() {
+    int N;
+    // 사용자 입력
+    scanf("%d", &N);
+    // 결과 출력
+    printf("%d\n", sequence(N));
+    return 0;
+}
